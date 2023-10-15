db47f037285c87d6524c87e5c4586cb4afaaae8d
LoveSy <shana@zju.edu.cn>
2023-10-15 11:11:05 +0800

    New way to unload zygisk (#51)
    * use old binder_rs
    
    * New way to unload zygisk (3a4fe53)
    
    Co-authored-by: LoveSy <shana@zju.edu.cn>
    Co-authored-by: 残页 <31466456+canyie@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: 5ec1cff <ewtqyqyewtqyqy@gmail.com>
    Co-authored-by: 残页 <31466456+canyie@users.noreply.github.com>

--- loader/src/injector/entry.cpp
+++ loader/src/injector/entry.cpp
@@ -7,15 +7,6 @@ using namespace std;
 
 void *self_handle = nullptr;
 
-[[gnu::destructor]] [[maybe_unused]]
-static void zygisk_cleanup_wait() {
-    if (self_handle) {
-        // Wait 10us to make sure none of our code is executing
-        timespec ts = { .tv_sec = 0, .tv_nsec = 10000L };
-        nanosleep(&ts, nullptr);
-    }
-}
-
 extern "C" [[gnu::visibility("default")]]
 void entry(void *handle) {
 #ifdef NDEBUG
