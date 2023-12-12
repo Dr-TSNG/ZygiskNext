1999ef734cf371589b10e57ec0d2affd436154c2
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2023-12-12 13:20:25 +0800

    fix ci upload (https://core.telegram.org/bots/api#markdownv2-style) test \_*[]()~`>#+-=|{}.!

--- .github/scripts/telegram_url.py
+++ .github/scripts/telegram_url.py
@@ -5,7 +5,10 @@ import urllib.parse
 url = f'https://api.telegram.org/bot{os.environ["BOT_TOKEN"]}'
 url += f'/sendMediaGroup?chat_id={urllib.parse.quote(os.environ["CHANNEL_ID"])}&media='
 
+# https://core.telegram.org/bots/api#markdownv2-style
 msg = os.environ["COMMIT_MESSAGE"]
+for c in ['\\', '_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']:
+    msg = msg.replace(c, f'\\{c}')
 commit_url = os.environ["COMMIT_URL"]
 commit_id = os.environ["COMMIT_ID"][:7]
 
