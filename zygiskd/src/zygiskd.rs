de2e19e89471409c9c60cd63d5f69d5d384db93c
Yervant7 <71306082+Yervant7@users.noreply.github.com>
2023-12-26 23:40:56 -0300

    Update zygiskd.rs

--- zygiskd/src/zygiskd.rs
+++ zygiskd/src/zygiskd.rs
@@ -43,7 +43,7 @@ pub fn main() -> Result<()> {
     {
         let mut msg = Vec::<u8>::new();
         let info = match root_impl::get_impl() {
-            root_impl::RootImpl::KernelSU | root_impl::RootImpl::Magisk => {
+            root_impl::RootImpl::KernelSU | root_impl::RootImpl::Magisk | root_impl::RootImpl::Kpatch => {
                 msg.extend_from_slice(&constants::DAEMON_SET_INFO.to_le_bytes());
                 let module_names: Vec<_> = modules.iter()
                     .map(|m| m.name.as_str()).collect();
