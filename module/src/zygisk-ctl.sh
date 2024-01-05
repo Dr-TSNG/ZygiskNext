381abc99d57a74dece1ae828f4cbfbef8b092bed
Nullptr <noreply@nullptr.icu>
2024-01-05 16:47:11 +0800

    Bump to 0.9.1

--- module/src/zygisk-ctl.sh
+++ module/src/zygisk-ctl.sh
@@ -3,7 +3,7 @@ MODDIR=${0%/*}/..
 if [ "$(which magisk)" ]; then
   export TMP_PATH="$(magisk --path)/zygisksu"
 else
-  export TMP_PATH="/debug_ramdisk/zygisksu"
+  export TMP_PATH="$(ksud path)/zygisksu"
 fi
 
 exec $MODDIR/bin/zygisk-ptrace64 ctl $*
